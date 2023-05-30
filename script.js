b509ed4f735ad9f79d893c5449811f8e51dca1d4
Re-Yan <reannub@gmail.com>
2023-05-30 09:54:12 +0800

    Implement Arithmetic Operations

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-const number = 2;
-console.log(number);
+let number1 = 10;
+let number2 = 5;
+let result = 0;
+
+const addition = (number1, number2) => {
+  result = number1 + number2;
+  return result;
+};
+const subtraction = (number1, number2) => {
+  result = number1 - number2;
+  return result;
+};
+const multiplication = (number1, number2) => {
+  result = number1 * number2;
+  return result;
+};
+const division = (number1, number2) => {
+  result = number1 / number2;
+  return result;
+};
+console.log(addition(number1, number2));
+console.log(subtraction(number1, number2));
+console.log(multiplication(number1, number2));
+console.log(division(number1, number2));
