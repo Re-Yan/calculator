0d7bcc470653d60939c80a870b67b3e47b379949
Re-Yan <reannub@gmail.com>
2023-05-31 20:01:03 +0800

    Create a For Each Loop for Adding Event Listeners
    Made a for each loop to add event listeners for 'numButtons' Nodelist

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let number1 = 10;
 let number2 = 5;
 let result = 0;
+const display = document.querySelector(".display");
+const numButtons = document.querySelectorAll(".numbers");
 
 const addition = (number1, number2) => {
   result = number1 + number2;
@@ -22,3 +24,13 @@ console.log(addition(number1, number2));
 console.log(subtraction(number1, number2));
 console.log(multiplication(number1, number2));
 console.log(division(number1, number2));
+
+// const displayValue = (numButtons) => {
+//   display.textContent = numButtons.value;
+// };
+
+numButtons.forEach((button) => {
+  button.addEventListener("click", displayValue);
+});
+console.log(typeof numButtons);
+console.log(numButtons);
